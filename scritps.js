c38108bde20c77b19832b9e4e569642c51b1c12c
Facundo Catania <elephob@gmail.com>
2021-05-07 23:52:07 -0300

    menus funcionales

--- scritps.js
+++ scritps.js
@@ -1,36 +1,55 @@
 document.addEventListener('DOMContentLoaded', () => {
 	fetchAll()
 	.then(f = ()  => {document.getElementById('main-loading').remove()})
-	.then(f = async() => listSort(pokemon_list, "Numeric"))
+	.then(f = async() => listSort(pokemon_list, "numeric"))
 	.then(f = async() => allTemplates(pokemon_list))
+	.then(f = async() => addEvents())
 })
 
 const POKEMON_KANTO = 151
 const pokemon_list = []
+var pokemon_filter = pokemon_list
 const search = document.getElementById("search-input")
-
-const listSort = (list, sortingBy) => {
-	switch (sortingBy) {
-		case "Numeric":
-			list.sort((a, b) => a.id - b.id)
-			break;
-		case "ReverseNumeric":
-			list.sort((a, b) => b.id - a.id)
-			break;
-		case "Alphabetic":
-			list.sort((a, b) => (a.name > b.name) ? 1 : -1)
-			break;
-		case "ReverseAlphabetic":
-			list.sort((a, b) => (a.name > b.name) ? -1 : 1)
-			break;
-	}
-}
 
 var countLoading = setInterval(() => {
 	if (pokemon_list.length < 151) {
 		document.getElementById('main-loading').innerHTML = `Cargando! ${pokemon_list.length}/151`
 	} else (clearInterval(countLoading))
 }, 25)
+
+const listSort = (list, sortingBy) => {
+	switch (sortingBy) {
+		case "numeric":
+			list.sort((a, b) => a.id - b.id)
+			break;
+		case "reverseNumeric":
+			list.sort((a, b) => b.id - a.id)
+			break;
+		case "alphabetic":
+			list.sort((a, b) => (a.name > b.name) ? 1 : -1)
+			break;
+		case "reverseAlphabetic":
+			list.sort((a, b) => (a.name > b.name) ? -1 : 1)
+			break;
+	}
+}
+
+const listFilter = (list, filterBy) => {
+	pokemon_filter = []
+	if (filterBy != "none") {
+		for (pokemon of list) {
+			if (pokemon.type1 === filterBy || pokemon.type2 === filterBy)
+			pokemon_filter.push(pokemon)
+		}
+		eraseTemplates()
+		allTemplates(pokemon_filter)
+	} else {
+		pokemon_filter = pokemon_list
+		listSort(pokemon_filter)
+		eraseTemplates()
+		allTemplates(pokemon_list)
+	}
+}
 
 async function fetchAll() {
 	for (let id=1; id<POKEMON_KANTO+1; id++) {
@@ -82,6 +101,7 @@ const renderTemplate = (pokemon) => {
 	const clone = template.cloneNode(true)
 	const fragment = document.createDocumentFragment()
 
+	clone.querySelector('.card').setAttribute('id', `${pokemon.name}`)
 	clone.querySelector('.card-body-img').setAttribute('src', pokemon.img)
 	clone.querySelector('.card-body-title').innerHTML = `${pokemon.name}`
 	clone.querySelector('.card-body-number').innerHTML = `No. ${idParse(pokemon.id)}`
@@ -97,10 +117,10 @@ const eraseTemplates = () => {
 	const templateContainer = document.querySelector('.flex')
 	templateContainer.innerHTML = "";
 }
-const searchValue = () => {
+const searchValue = (list) => {
 	const pokemon_search = []
 	const searchIndex = search.value.length
-	for (pokemon of pokemon_list) {
+	for (pokemon of list) {
 		if (pokemon.name.substr(0, searchIndex) === search.value.toLowerCase()){
 			pokemon_search.push(pokemon)
 		}
@@ -115,38 +135,53 @@ const searchBarUnderline = (letters) => {
 	}
 	toggleSearchBarUnderline(letters)
 }
-const downSearchBarUnderline = (space) => {
-		document.querySelector(`#search-bar-underline-${space}`).classList.remove("underline-top")
-		document.querySelector(`#search-bar-underline-${space}`).classList.add("underline-down")
+const downSearchBarUnderline = (letterQuant) => {
+		document.querySelector(`#search-bar-underline-${letterQuant}`).classList.remove("underline-top")
+		document.querySelector(`#search-bar-underline-${letterQuant}`).classList.add("underline-down")
 }
-const toggleSearchBarUnderline = (space) => {
-	document.querySelector(`#search-bar-underline-${space}`).classList.toggle("underline-down")
-	document.querySelector(`#search-bar-underline-${space}`).classList.toggle("underline-top")
+const toggleSearchBarUnderline = (letterQuant) => {
+	document.querySelector(`#search-bar-underline-${letterQuant}`).classList.toggle("underline-down")
+	document.querySelector(`#search-bar-underline-${letterQuant}`).classList.toggle("underline-top")
+	closeOtherMenu('search-bar-sort-menu')
+	closeOtherMenu('search-bar-filter-menu')
 }
-const toggleMenu = (menu) => {
+const toggleMenu = (menu, otherMenu) => {
 	document.getElementById(menu).classList.toggle("hidden")
 	document.getElementById(menu).classList.toggle("border-menu")
-}
-const pickOptionMenu = (button, target) => {
-	document.getElementById(button).childNodes[0].textContent = target.target.innerHTML
-	eraseTemplates()
-	allTemplates(pokemon_list)
-	// document.getElementById(button).childNodes[0].textContent = option
+	closeOtherMenu(otherMenu)
 }
 
-// const sortBy = (target) => {
-// 	listSort(pokemon_list,)
-// }
-// const pokemonClickId = () => {
+const closeOtherMenu = (menu) => {
+	if (!document.getElementById(menu).classList.contains('hidden')) {	
+		document.getElementById(menu).classList.toggle("hidden")
+		document.getElementById(menu).classList.toggle("border-menu")
+	}
+}
 
-// }
+const pickOptionMenu = (button, option) => {
+	document.getElementById(button).childNodes[0].textContent = option.target.innerHTML
+	switch (button) {
+		case 'search-bar-sort-button':
+			listSort(pokemon_list, option.path[0].getAttribute('id'))
+			listSort(pokemon_filter, option.path[0].getAttribute('id'))
+			eraseTemplates()
+			allTemplates(pokemon_filter)
+			break;
+		case 'search-bar-filter-button':
+			listFilter(pokemon_list, option.path[0].getAttribute('id'))
+			break;
+	}
+}
 
-document.getElementById("search-input").addEventListener("focus", () => toggleSearchBarUnderline(search.value.length+1))
-document.getElementById("search-input").addEventListener("blur", () => toggleSearchBarUnderline(search.value.length+1))
-document.getElementById("search-input").addEventListener("input", () => searchValue())
-document.getElementById('search-bar-sort-button').addEventListener("click", () => toggleMenu('search-bar-sort-menu'))
-document.getElementById('search-bar-filter-button').addEventListener("click", () => toggleMenu('search-bar-filter-menu'))
-document.getElementById('search-bar-sort-menu').addEventListener("click", (x) => listSort(pokemon_list, x.path[0].getAttribute('id')))
-document.getElementById('search-bar-sort-menu').addEventListener("click", (x) => pickOptionMenu('search-bar-sort-button', x))
+const addEvents = () => {
+	document.getElementById("search-input").addEventListener("focus", () => toggleSearchBarUnderline(search.value.length+1))
+	document.getElementById("search-input").addEventListener("blur", () => toggleSearchBarUnderline(search.value.length+1))
+	document.getElementById("search-input").addEventListener("input", () => searchValue(pokemon_filter))
+	document.getElementById('search-bar-sort-button').addEventListener("click", () => toggleMenu('search-bar-sort-menu','search-bar-filter-menu'))
+	document.getElementById('search-bar-filter-button').addEventListener("click", () => toggleMenu('search-bar-filter-menu','search-bar-sort-menu'))
+	document.getElementById('search-bar-sort-options').addEventListener("click", (x) => pickOptionMenu('search-bar-sort-button', x))
+	document.getElementById('search-bar-filter-options').addEventListener("click", (x) => pickOptionMenu('search-bar-filter-button', x))
 
-// document.getElementById("pokemon-click").addEventListener("click", (x) => console.log(x.path[0].innerHTML))
+	document.getElementById("pokemon-click").addEventListener("click", (x) => console.log(x))
+
+}
